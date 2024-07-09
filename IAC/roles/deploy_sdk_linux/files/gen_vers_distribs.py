55f19c342404fc63a4c403958c486e83a11825de
fisher772 <kovachvladislav99@gmail.com>
2024-07-09 14:25:13 +0500

    v6.7

--- IAC/roles/deploy_sdk_linux/files/gen_vers_distribs.py
+++ IAC/roles/deploy_sdk_linux/files/gen_vers_distribs.py
@@ -4,26 +4,15 @@ import requests
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
 
-<<<<<<< HEAD
-# Куда будет импортирован дата файл со списком
-dest_path = '/home/ansible/ansible/projects/IAC/playbooks/roles/deploy_sdk_linux/vars/vers_distribs.yml'
-
-# Ранжируем список в перемнных
-=======
 # Where the data file with the list will be imported from
 dest_path = '/home/ansible/ansible/projects/IAC/roles/deploy_sdk_linux/vars/vers_distribs.yml'
 
 # Ranking lists in variables
->>>>>>> devops
 centos = list(range(7, 10))
 rhel = list(range(7, 10))
 fedora = list(range(24, 41))
 
-<<<<<<< HEAD
-# Создаем пустой словарь для хранения значений
-=======
 # Creating an empty dictionary for storing values
->>>>>>> devops
 versions = {
     "centos_vers": centos,
     "rhel_vers": rhel,
@@ -42,25 +31,6 @@ for url, key in [(debian, 'debian_vers'), (ubuntu, 'ubuntu_vers')]:
     except Exception as err:
         print(f'Other error occurred: {err}') # Python 3.6
     else:
-<<<<<<< HEAD
-        # Создаем объект BeautifulSoup для парсинга HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-        # Находим все теги <a> внутри тега <body>
-        a_tags = soup.find_all('a')
-        # Создаем пустой список для хранения значений для текущего URL
-        values = []
-        # Извлекаем текст каждого тега <a> и добавляем в список, если он не содержит символов '/' и одинарные кавычки, и не ведет к родительскому каталогу
-        for a in a_tags:
-            href = a.get('href')
-            if href and not href.startswith('../'):
-                # Удаляем лишние символы '/' и одинарные кавычки
-                cleaned_href = href.replace('/', '').replace("'", "")
-                values.append(cleaned_href)
-        # Добавляем список значений в словарь с ключом, соответствующим текущему URL
-        versions[key] = values
-
-# Форматируем данные вручную для сохранения в файл .yml
-=======
         # Create a BeautifulSoup object for parsing HTML
         soup = BeautifulSoup(response.text, 'html.parser')
         # Find all <a> tags inside the <body> tag
@@ -78,24 +48,15 @@ for url, key in [(debian, 'debian_vers'), (ubuntu, 'ubuntu_vers')]:
         versions[key] = values
 
 # Manually format data for saving in a .yml file
->>>>>>> devops
 formatted_data = ""
 for key, values in versions.items():
     formatted_data += f"{key}: {values}\n"
 
-<<<<<<< HEAD
-# Сохраняем форматированные данные в файл .yml
-with open(dest_path, 'w') as file:
-    file.write(formatted_data)
-
-# Устанавливаем гранты и владельца дата файла 
-=======
 # Save the formatted data in a .yml file
 with open(dest_path, 'w') as file:
     file.write(formatted_data)
 
 # Set permissions and owner of the data file
->>>>>>> devops
 uid = pwd.getpwnam('ansible').pw_uid
 gid = pwd.getpwnam('ansible').pw_gid
 
